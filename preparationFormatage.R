6e82c866b26c7f78e994495da06ddae76f81738f
clément J <87208881+clementcaen@users.noreply.github.com>
2023-06-13 11:57:42 +0200

    1.3prerparation_debut1.5
    1.3

--- preparationFormatage.R
+++ preparationFormatage.R
@@ -8,7 +8,7 @@ data <- stat_acc_V3
 convertDate <- function (colonneCsv){
   new_date <- NULL
   for (each_date in colonneCsv){
-    new_date <- append(new_date, as.Date(each_date, format = "%Y-%m-%d %H:%M:%S")) # format represente les dates venant du csv ex:2009-10-04 20:50:00
+    new_date <- append(new_date, as.POSIXct(each_date, format = "%Y-%m-%d %H:%M:%S")) # format represente les dates venant du csv ex:2009-10-04 20:50:00
   }
   return(new_date)
 }
@@ -19,6 +19,7 @@ convertNumeric <- function (colonneCsv){
   }
   return(new_num)
 }
+
 # conversion colonnes non multi-modales
 data["Num_Acc"] <- convertNumeric(data["Num_Acc"])
 data["id_usa"] <- convertNumeric(data["id_usa"])
@@ -33,5 +34,32 @@ data["place"] <- convertNumeric(data["place"])
 print(data)
 print("Conversion réussi")
 print("CHLOE")
+
+# clément
+# Construire un jeu de données avec le nombre d’accidents selon la gravité pour 100.000
+#habitants par région (qui servirait à l’ACP discutée dans la section 3 intitulée Analyse de
+#                      données ci-dessous)
+# on creer un tableau avec les données du nombre d'habitant par commune https://www.insee.fr/fr/statistiques/2044741
+stat_pop <- read.csv("base-cc-evol-struct-pop-2009.csv", sep=';')
+print(stat_pop)
+# P09_POP: population | CODGEO: code insee | REG: id_region
+# On groupe par région avec la sommme des habitants des villes (total d'habitant par région) et les divisant par 100 000 pour avoir les données pour 100 000 habitant
+total_pop <- aggregate(P09_POP ~ REG, data = stat_pop, FUN = sum)
+pop100000_by_reg <- NULL
+pop100000_by_reg = data.frame(total_pop$REG, total_pop$P09_POP /100000)
+print(pop100000_by_reg)
+# On trouve tous les accidents dans la région en mergeant les 2 tableaux
+names(pop100000_by_reg)[pop100000_by_reg == "CODGEO"] <- "id_code_insee"
+merging_tableau <- merge(total_pop, data, by="id_code_insee")
+print(merging_tableau)
+# on groupe par région les accidents
+accid_by_reg <- aggregate(place ~ REG, data = merging_tableau, FUN = sum) # on somme les personnes touchés par l'accidents (place) pour connaitre le nombre d'affecté par les accident par région
+print(accid_by_reg)
+# On trie par gravité ? (histogramme)
+
+
+
+
+
 
 
