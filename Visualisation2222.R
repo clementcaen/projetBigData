e7a07f984709bf228d2994a8df233b2804d5b898
chlobg <136447267+chlobg@users.noreply.github.com>
2023-06-16 09:22:00 +0200

    okay
    okay

---
+++ Visualisation2222.R
@@ -0,0 +1,62 @@
+
+
+# 1.Créer des représentations graphiques pour:
+
+ 
+  #Nombre d’accidents en fonction des conditions atmosphériques
+    #Marzhin
+ 
+
+
+  # Nombre d’accidents en fonction de la description de la surface
+    #Marzhin
+
+
+
+  # Nombre d’accidents selon la gravité
+    #Marzhin
+
+
+
+  # Nombre d’accidents par tranches d’heure
+    #Clément
+
+
+
+  # Nombre d’accidents par ville
+    #Clément
+
+
+
+#2.Créer des histogrammes 
+
+
+# Quantité d’accidents en fonction des tranches d’âges
+  #Chloé
+
+
+
+# Moyenne mensuelle des accidents
+  #Chloé
+
+# Proposer une représentation sous formes de carte de la quantité d’accidents enregistrés par
+  #Clément
+
+  #région 
+
+
+
+  #départements
+
+
+
+# Même chose avec les taux d’accidents graves
+  #Clément
+
+  #région
+
+
+  #départements
+
+
+
